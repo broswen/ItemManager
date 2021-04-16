1d66572abc424aa0eb9ea7c09f023f7e909c7f43
brad <broswen@gmail.com>
2021-04-15 23:06:13 -0500

    filter dynamodb stream on INSERT

--- src/lambdas/reporting/ReportsStreamWatcher.ts
+++ src/lambdas/reporting/ReportsStreamWatcher.ts
@@ -10,6 +10,11 @@ module.exports.handler = async (event: DynamoDBStreamEvent) => {
 
   event.Records.forEach(record => console.log(record))
   for (let record of event.Records) {
+
+    // only process events that are new reports and not modifications of existing reports
+    // prevents workflow from triggering itself, ask me how I know.
+    if (record.eventName !== 'INSERT') continue
+
     const id: string = record.dynamodb.Keys.PK.S.split('#')[1]
     console.log(`generating report ${id}`)
     const params: StartExecutionCommandInput = {
